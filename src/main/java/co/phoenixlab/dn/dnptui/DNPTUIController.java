d3515e9a1f2aa2c5a72d781ed9f392866312fbcd
Vincent Zhang <vzhang@purdue.edu>
2015-04-23 15:41:01 -0400

    Added handlers to toolbar buttons

--- src/main/java/co/phoenixlab/dn/dnptui/DNPTUIController.java
+++ src/main/java/co/phoenixlab/dn/dnptui/DNPTUIController.java
@@ -34,9 +34,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 public class DNPTUIController {
 
@@ -45,11 +52,7 @@ public class DNPTUIController {
     private Scene scene;
 
     @FXML private Label titleLbl;
-    @FXML private Button exitBtn;
     @FXML private Button maxRestoreBtn;
-    @FXML private Button iconifyBtn;
-    @FXML private Button openPakBtn;
-    @FXML private Button openVirtualPakBtn;
     @FXML private Button findBtn;
     @FXML private Button exportBtn;
     @FXML private Button exportFolderBtn;
@@ -57,10 +60,15 @@ public class DNPTUIController {
 
     private final BooleanProperty noPakLoadedProperty;
     private final StringProperty openedFilePathProperty;
+    private final BooleanProperty maximizedProperty;
+
+    private Path lastOpenedDir;
 
     public DNPTUIController() {
         noPakLoadedProperty = new SimpleBooleanProperty(this, "noPakLoaded", true);
         openedFilePathProperty = new SimpleStringProperty(this, "openedFilePath", "No File");
+        maximizedProperty = new SimpleBooleanProperty(this, "maximized", false);
+        lastOpenedDir = Paths.get(System.getProperty("user.dir"));
     }
 
     public void setStageSceneApp(Stage stage, Scene scene, DNPTApplication application) {
@@ -71,11 +79,15 @@ public class DNPTUIController {
     }
 
     public void init() {
+        maximizedProperty.bind(stage.maximizedProperty());
         findBtn.disableProperty().bind(noPakLoadedProperty);
         exportBtn.disableProperty().bind(noPakLoadedProperty);
         exportFolderBtn.disableProperty().bind(noPakLoadedProperty);
         closePakBtn.disableProperty().bind(noPakLoadedProperty);
         titleLbl.textProperty().bind(Bindings.concat("DN Pak Tool - ").concat(openedFilePathProperty));
+        maximizedProperty.addListener((observable, oldValue, newValue) -> {
+            maxRestoreBtn.setId(newValue ? "window-restore-button" : null);
+        });
 
         FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.125D), scene.getRoot());
         fadeTransition.setFromValue(0D);
@@ -111,5 +123,58 @@ public class DNPTUIController {
 
     public void iconify(ActionEvent event) {
         stage.setIconified(true);
+    }
+
+    public void toggleMax(ActionEvent event) {
+        boolean old = stage.isMaximized();
+        stage.setMaximized(!old);
+    }
+
+    public void openPak(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(lastOpenedDir.toFile());
+        fileChooser.setTitle("Choose a Pak file");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Dragon Nest Package File", "*.pak"));
+        Optional.ofNullable(fileChooser.showOpenDialog(stage)).
+                map(File::toPath).
+                ifPresent(this::loadPak);
+    }
+
+    private void loadPak(Path path) {
+        lastOpenedDir = path.getParent();
+
+    }
+
+    public void openVirtualPak(ActionEvent event) {
+        DirectoryChooser dirChooser = new DirectoryChooser();
+        dirChooser.setInitialDirectory(lastOpenedDir.toFile());
+        dirChooser.setTitle("Choose a Pak file");
+        Optional.ofNullable(dirChooser.showDialog(stage)).
+                map(File::toPath).
+                ifPresent(this::loadPak);
+    }
+
+    private void loadVirtualPak(Path dir) {
+        lastOpenedDir = dir;
+        //  Show loading dialog
+
+        //  Dispatch job
+
+    }
+
+    public void find(ActionEvent event) {
+
+    }
+
+    public void exportFile(ActionEvent event) {
+
+    }
+
+    public void exportFolder(ActionEvent event) {
+
+    }
+
+    public void closePak(ActionEvent event) {
+
     }
 }
