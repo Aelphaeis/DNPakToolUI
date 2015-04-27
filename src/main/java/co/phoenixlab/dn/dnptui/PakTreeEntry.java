d2f4157a857ef233637ffba00e391c721b0acbc0
Vincent Zhang <vzhang@purdue.edu>
2015-04-27 14:57:33 -0400

    PakTreeEntry datastore for treeview

---
+++ src/main/java/co/phoenixlab/dn/dnptui/PakTreeEntry.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Vincent Zhang/PhoenixLAB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package co.phoenixlab.dn.dnptui;
+
+import co.phoenixlab.dn.pak.FileEntry;
+import co.phoenixlab.dn.pak.PakFile;
+
+import java.nio.file.Path;
+
+public class PakTreeEntry {
+
+    public final String name;
+    public final Path path;
+    public final FileEntry entry;
+    public final PakFile parent;
+
+    public PakTreeEntry(String name, Path path, FileEntry entry, PakFile parent) {
+        this.name = name;
+        this.path = path;
+        this.entry = entry;
+        this.parent = parent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PakTreeEntry that = (PakTreeEntry) o;
+        return name.equals(that.name) && path.equals(that.path);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + path.hashCode();
+        return result;
+    }
+}
