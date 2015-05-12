811143cb0f9602951a5867aa948b6c5286f015f4
Vincent Zhang <vzhang@purdue.edu>
2015-05-12 19:09:17 -0400

    FEAT: Added sprite animation spinner

---
+++ src/main/java/co/phoenixlab/dn/dnptui/SpriteAnimation.java
@@ -0,0 +1,86 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Vincent Zhang/PhoenixLAB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package co.phoenixlab.dn.dnptui;
+
+import javafx.animation.Interpolator;
+import javafx.animation.Transition;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+/**
+ * SpriteAnimation from http://blog.netopyr.com/2012/03/09/creating-a-sprite-animation-with-javafx/
+ */
+
+public class SpriteAnimation extends Transition {
+
+    private final ImageView imageView;
+    private final int count;
+    private final int columns;
+    private final int offsetX;
+    private final int offsetY;
+    private final int width;
+    private final int height;
+
+    private int lastIndex;
+
+    public SpriteAnimation(
+            ImageView imageView,
+            Duration duration,
+            int count,   int columns,
+            int offsetX, int offsetY,
+            int width,   int height) {
+        this(imageView, duration, count, columns, offsetX, offsetY, width, height, 15);
+    }
+
+    public SpriteAnimation(
+            ImageView imageView,
+            Duration duration,
+            int count,   int columns,
+            int offsetX, int offsetY,
+            int width,   int height, int fps) {
+        super(fps);
+        this.imageView = imageView;
+        this.count     = count;
+        this.columns   = columns;
+        this.offsetX   = offsetX;
+        this.offsetY   = offsetY;
+        this.width     = width;
+        this.height    = height;
+        setCycleDuration(duration);
+        setInterpolator(Interpolator.LINEAR);
+    }
+
+    protected void interpolate(double k) {
+        final int index = Math.min((int) Math.floor(k * count), count - 1);
+        if (index != lastIndex) {
+            final int x = (index % columns) * width  + offsetX;
+            final int y = (index / columns) * height + offsetY;
+            imageView.setViewport(new Rectangle2D(x, y, width, height));
+            lastIndex = index;
+        }
+    }
+}
+
