Aelphaeis/DNPakToolUI

commit 416b775385aa6d23beaf0015bc35dfa6b8489928

REFACTOR: Extracted FadeTransition to util

src/main/java/co/phoenixlab/dn/dnptui/DNPTUIController.java
@@ -24,11 +24,10 @@
 
 package co.phoenixlab.dn.dnptui;
 
+import co.phoenixlab.dn.dnptui.fx.FadeTransitionUtil;
 import co.phoenixlab.dn.dnptui.fx.SpriteAnimation;
 import co.phoenixlab.dn.pak.DNPakTool;
 import javafx.animation.Animation;
-import javafx.animation.FadeTransition;
-import javafx.animation.Interpolator;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.*;
@@ -267,11 +266,8 @@ public class DNPTUIController {
                         bind(root.heightProperty().subtract(126)));
 
         //  Fade the window in
-        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.25D), scene.getRoot());
-        fadeTransition.setInterpolator(Interpolator.EASE_OUT);
-        fadeTransition.setFromValue(0D);
-        fadeTransition.setToValue(1D);
-        fadeTransition.playFromStart();
+        FadeTransitionUtil.fadeTransitionIn(Duration.seconds(0.25D), scene.getRoot()).
+                play();
     }
 
     /**
@@ -305,12 +301,8 @@ public class DNPTUIController {
      * @param dummy Any value (including null). Only present to allow for method reference in lambda
      */
     private void quit(Object dummy) {
-        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5D), scene.getRoot());
-        fadeTransition.setInterpolator(Interpolator.EASE_IN);
-        fadeTransition.setFromValue(1D);
-        fadeTransition.setToValue(0D);
-        fadeTransition.setOnFinished(e -> application.stop());
-        fadeTransition.playFromStart();
+        FadeTransitionUtil.fadeTransitionOut(Duration.seconds(0.5D), scene.getRoot(), application::stop).
+                play();
     }
 
     /**
@@ -446,30 +438,22 @@ public class DNPTUIController {
         ChangeListener<Number> yPosListener = (observable, oldValue, newValue) ->
                 loadingStage.setY(newValue.doubleValue() + stage.getHeight() / 2 - loadingStage.getHeight() / 2);
         task.setOnSucceeded(e -> {
-            FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5D), scene.getRoot());
-            fadeTransition.setInterpolator(Interpolator.EASE_IN);
-            fadeTransition.setFromValue(1D);
-            fadeTransition.setToValue(0D);
-            fadeTransition.setOnFinished(ae -> {
+            FadeTransitionUtil.fadeTransitionOut(Duration.seconds(0.5D), scene.getRoot(), () -> {
                 //  Stop animations, destroy window, remove listeners
                 spriteAnimation.stop();
                 loadingStage.close();
                 infoLbl.textProperty().unbind();
                 stage.xProperty().removeListener(xPosListener);
                 stage.yProperty().removeListener(yPosListener);
-            });
-            fadeTransition.playFromStart();
+            }).play();
         });
         stage.xProperty().addListener(xPosListener);
         stage.yProperty().addListener(yPosListener);
         //  Display
+        scene.getRoot().setOpacity(0D);
         loadingStage.show();
-        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.25D), scene.getRoot());
-        fadeTransition.setInterpolator(Interpolator.EASE_OUT);
-        fadeTransition.setFromValue(0D);
-        fadeTransition.setToValue(1D);
-        fadeTransition.playFromStart();
-        spriteAnimation.play();
+        FadeTransitionUtil.fadeTransitionIn(Duration.seconds(0.25D), scene.getRoot()).
+                play();
         //  Center the window
         xPosListener.changed(null, null, stage.getX());
         yPosListener.changed(null, null, stage.getY());

src/main/java/co/phoenixlab/dn/dnptui/fx/FadeTransitionUtil.java
@@ -1,0 +1,65 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Vincent Zhang/PhoenixLAB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package co.phoenixlab.dn.dnptui.fx;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+
+public class FadeTransitionUtil {
+
+    public static FadeTransition fadeTransitionOut(Duration duration, Node node) {
+        return fadeTransitionOut(duration, node, null);
+    }
+
+    public static FadeTransition fadeTransitionOut(Duration duration, Node node, Runnable onFinished) {
+        FadeTransition fadeTransition = new FadeTransition(duration, node);
+        fadeTransition.setInterpolator(Interpolator.EASE_IN);
+        fadeTransition.setFromValue(1D);
+        fadeTransition.setToValue(0D);
+        if (onFinished != null) {
+            fadeTransition.setOnFinished(e -> onFinished.run());
+        }
+        return fadeTransition;
+    }
+
+    public static FadeTransition fadeTransitionIn(Duration duration, Node node) {
+        return fadeTransitionIn(duration, node, null);
+    }
+
+    public static FadeTransition fadeTransitionIn(Duration duration, Node node, Runnable onFinished) {
+        FadeTransition fadeTransition = new FadeTransition(duration, node);
+        fadeTransition.setInterpolator(Interpolator.EASE_OUT);
+        fadeTransition.setFromValue(0D);
+        fadeTransition.setToValue(1D);
+        if (onFinished != null) {
+            fadeTransition.setOnFinished(e -> onFinished.run());
+        }
+        return fadeTransition;
+    }
+
+}

commit c6339765b07844894c96b5e5830232b9ef140843

FIX: Fixed spinner not animating
Fixed bug introduced in 416b775385aa6d23beaf0015bc35dfa6b8489928

src/main/java/co/phoenixlab/dn/dnptui/DNPTUIController.java
@@ -452,6 +452,7 @@ public class DNPTUIController {
         //  Display
         scene.getRoot().setOpacity(0D);
         loadingStage.show();
+        spriteAnimation.playFromStart();
         FadeTransitionUtil.fadeTransitionIn(Duration.seconds(0.25D), scene.getRoot()).
                 play();
         //  Center the window